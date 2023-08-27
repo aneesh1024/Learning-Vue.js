25de813e444617c5531beba3dfd438d0811cc101
Aneesh Sharma <as416106@gmail.com>
2023-08-27 18:35:38 +0530

    Completed Vue if else conditions

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,17 @@
 <template>
-  <div>
-    <div>{{ greet }} {{ name }}</div> <!-- 1st way to add dynamic text by using the mustache syntax -->
-    <div v-html="hack"></div> <!-- 2nd way is by using v-bind director  -->
-    <h2 :id="headingId">Heading</h2>
-    <button :disabled="isDisabled">Bind</button>
-    <h2 class="underline" >Underlined text</h2>
-    <h2 :class="status" class="underline">Status</h2>
-    <h2 class="danger" :class="isSafe && 'success' " >Safe</h2>
-    <h2 :class="isSafe?'success':'danger'" > {{ isSafe?'success':'danger' }}</h2>
-    <h2 :class="['underline','success']" >New</h2>
-    <p :class="[ isBig && 'big',isSafe?'success':'danger']" >Conditional Array</p>
-    <h2 :class="{
-      big:isBig,
-      success:isSafe,
-      'danger':!isSafe
-    }" >COMPLEX</h2>
-    <h2 :style="{
-      color:highlightColor,
-      fontSize:headerSize+'rem',
-      padding:'10px'
-    }" >Inline Style</h2>
-    <h2 :style="headerStyleObject" >Object Styling</h2>
-    <div :style="[baseStyleObject, successStyleObject]">Success Style</div>
-    <div :style="[baseStyleObject, dangerStyleObject]">Danger Style</div>
-  </div >
+    <h2 v-if="num === 0">Number is zero</h2>
+    <h2 v-else-if="num > 0">Number is greater than 0</h2>
+    <h2 v-else-if="num < 0">Number is less than zero</h2>
+    <h2 v-else>Not a Number</h2>
+
+    <template v-if="display">
+      <h2>Aneesh</h2>
+      <h2>Techfluencers</h2>
+      <h2>Vue</h2>
+    </template>
+
+    <h2 v-show="show">Using v-show</h2>
+    <h2 v-if="show">Using v-show</h2>
 </template>
 <script>
 
@@ -31,37 +19,9 @@ export default {
   name: 'App',
   data() {
     return {
-      greet:'Yo',
-      name:"Batman",
-      hack:`<a href="#" onclick="alert('you have been hacked')">getsuga tenshou</a>`,
-      headingId:'heading',
-      status:"success",
-      isDisabled:false,
-      isSafe:true,
-      isBig:false,
-      big:'big',
-      highlightColor:'orange',
-      headerSize:4,
-      headerStyleObject:{
-        color: 'orange',
-        fontSize: '3rem',
-        padding: '10px'
-      },
-      baseStyleObject:{
-        fontSize:'50px',
-        padding:'10px'
-      },
-      successStyleObject:{
-        color:'green',
-        backgroundColor:'tomato',
-        border:'3px dashed green',
-        fontSize:'70px'
-      },
-      dangerStyleObject:{
-        color:'red',
-        backgroundColor:'#4a4ae6',
-        border:'4px solid darkred'
-      }
+      num:'hello',
+      display:false,
+      show:false,
     }
   },
 }
